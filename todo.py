184322cff23319c8518f8fbdc495466ba270421e
Andrew <lyfeshomebrew@gmail.com>
2019-05-20 22:40:28 -0700

    fixed gitignore to ignore user settings file

---
+++ todo.py
@@ -0,0 +1,17 @@
+todo:
+
+
+settings:
+(download by default)
+window launch size
+double click for more info
+
+
+
+
+pillow on-demand
+injector remembers last selection
+double-click payloads brings up old versions
+npprmint.github.io
+
+blank button when no repo page is available
